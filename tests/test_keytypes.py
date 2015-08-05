da37d30cc60386b07b156c30e1e27b2f053b7f7b
cy18 <cy9841@gmail.com>
2015-08-05 14:19:37 -0400

    Added some testing using tuple and list as key

--- tests/test_keytypes.py
+++ tests/test_keytypes.py
@@ -10,13 +10,33 @@ import unittest
 import sqlitedict2
 
 class KeyTypeTest(unittest.TestCase):
-    fname = 'tests/db/sqlitedict2-mutable-test.sqlite'
     def test_list_key(self):
-        """Use mutable object as key"""
+        """Use list object as key"""
+        fname = 'tests/db/sqlitedict2-mutable-test.sqlite'
         d = sqlitedict2.SqliteDict(fname, autocommit=True)
-        d[[1,2]] = 3
-        self.assetEqual(d[self.d[[1,2]]], 3)
-        d[[1,2,3]] = 4
-        self.assetInequal(d[self.d[[1,2]]], 3)
-        self.assetEqual(d[self.d[[1,2]]], 4)
+        k1 = [1,2]
+        k2 = k1.copy()
+        d[k1] = 3
+        self.assertEqual(d[k1], 3)
+        self.assertEqual(d[k2], 3)
+        k1.append('3')
+        d[k1] = 4
+        self.assertEqual(d[k1], 4)
+        self.assertEqual(d[k2], 3)
         
+    def test_float_tuple_key(self):
+        """Use float tuple object as key
+        
+        k1 == k2 but k1 is not k2
+        """
+        fname = 'tests/db/sqlitedict2-mutable-test.sqlite'
+        d = sqlitedict2.SqliteDict(fname, autocommit=True)
+        k1 = (1.1, 2.2)
+        k2 = (1.1, 2.2)
+        self.assertIsNot(k1, k2)
+        d[k1] = 3
+        self.assertEqual(d[k1], 3)
+        self.assertEqual(d[k2], 3)
+        d[k2] = 4
+        self.assertEqual(d[k1], 4)
+        self.assertEqual(d[k2], 4)
